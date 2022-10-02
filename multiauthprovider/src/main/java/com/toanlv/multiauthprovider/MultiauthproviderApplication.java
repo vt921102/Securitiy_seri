901cd1fe4810245a707a5d133a8af14dddea3507
Toan Le <toan.le@ataccama.com>
2022-10-02 16:44:38 +0700

    add gradle project lesson 6

---
+++ multiauthprovider/src/main/java/com/toanlv/multiauthprovider/MultiauthproviderApplication.java
@@ -0,0 +1,13 @@
+package com.toanlv.multiauthprovider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiauthproviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultiauthproviderApplication.class, args);
+    }
+
+}
---
+++ multiauthprovider/src/test/java/com/toanlv/multiauthprovider/MultiauthproviderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toanlv.multiauthprovider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiauthproviderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
